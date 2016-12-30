73a356c449a42abae2dd06255756f1a89c1f9972
Kristof Ringleff <kristof@fooman.co.nz>
2016-12-31 12:23:05 +1300

    fix use correct module in test, remove unnecessary use lines

--- tests/functional/tests/app/Fooman/GoogleAnalyticsPlus/Test/Block/Onepage/Body.php
+++ tests/functional/tests/app/Fooman/GoogleAnalyticsPlus/Test/Block/Onepage/Body.php
@@ -1,19 +1,16 @@
 <?php
 /**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
+ * @author     Kristof Ringleff
+ * @copyright  Copyright (c) 2015 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
-
+ 
 namespace Fooman\GoogleAnalyticsPlus\Test\Block\Onepage;
 
-use Magento\Checkout\Test\Fixture\Checkout;
 use Magento\Mtf\Block\Block;
-use Magento\Mtf\Client\Locator;
 
-/**
- * Class Success
- * One page checkout success block
- */
 class Body extends Block
 {
 
